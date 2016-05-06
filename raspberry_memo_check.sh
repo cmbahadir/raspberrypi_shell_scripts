bb088eb916bff4f5f1a2092864b7bac34bd8af73
cmbahadir <c.mete.bahadir@gmail.com>
2016-05-06 19:15:20 +0300

    Create raspberry_memo_check.sh
    This script checks the memory and running apt-get processes that may cause lack of memory.

---
+++ raspberry_memo_check.sh
@@ -0,0 +1,29 @@
+!/bin/bash
+#Author: Cihan Mete BAHADIR
+#Date: 06.05.2016
+#License: Free of anything.
+
+upd=$(ps -ef | grep "update" | wc -l)
+upg=$(ps -ef | grep "upgrade" | wc -l)
+ins=$(ps -ef | grep "upgrade" | wc -l)
+sizeofdisk=$(df -h | grep mmcblk0p1 | awk '{print $5}' | cut -d'%' -f1)
+
+function memo
+{
+if [ $sizeofdisk -gt 95 ]; then 
+                echo  "Its almost full:" 
+                echo  "$sizeofdisk"
+                exit 0
+else
+                echo  "OK"
+                echo  "$sizeofdisk"
+                exit 1
+fi
+}
+
+if [[ ("$upd" > 1) || ("$upg" > 1) || ("$ins" > 1) ]]; then 
+        memo
+else 
+        echo "There is no apt-get process running."
+        exit 0
+fi
