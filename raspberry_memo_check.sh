cdd2aef162f1da6f9a12d7700f64e5ded3d4b0f7
cmbahadir <c.mete.bahadir@gmail.com>
2016-05-06 19:36:36 +0300

    memory using apt-get process killed.
    This script checks for the size of /dev/mmcblk0p1 and removes "apt-get process" when the available memory size reaches 5%.

--- raspberry_memo_check.sh
+++ raspberry_memo_check.sh
@@ -1,4 +1,4 @@
-!/bin/bash
+#!/bin/bash
 #Author: Cihan Mete BAHADIR
 #Date: 06.05.2016
 #License: Free of anything.
@@ -13,6 +13,8 @@ function memo
 if [ $sizeofdisk -gt 95 ]; then 
                 echo  "Its almost full:" 
                 echo  "$sizeofdisk"
+                process_id=$(ps -fu $USER| grep "apt-get" | grep -v "grep" | awk '{print $2}')
+                kill -9 $process_id
                 exit 0
 else
                 echo  "OK"
@@ -23,6 +25,7 @@ fi
 
 if [[ ("$upd" > 1) || ("$upg" > 1) || ("$ins" > 1) ]]; then 
         memo
+        echo "Process killed."
 else 
         echo "There is no apt-get process running."
         exit 0
